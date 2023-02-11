15bbad42684531bee8d80d67f19d9eb7be08eb55
Elisa <elisagonzalez@live.nl>
2023-02-11 21:16:36 +0100

    added selectors for date and time

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+
+losAngelesDateElement.innerHTML = `February 11th 2023`;
+losAngelesTimeElement.innerHTML = `21:04:23 <small>PM</small>`;
+
+let amsterdamElement = document.querySelector("#amsterdam");
+let amsterdamDateElement = amsterdamElement.querySelector(".date");
+let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+
+amsterdamDateElement.innerHTML = `February 11th 2023`;
+amsterdamTimeElement.innerHTML = `21:04:23 <small>PM</small>`;
+
+let tokyoElement = document.querySelector("#tokyo");
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+
+tokyoDateElement.innerHTML = `February 11th 2023`;
+tokyoTimeElement.innerHTML = `21:04:23 <small>PM</small>`;
