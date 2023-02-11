6e3810740b6b3a7fe57e21ffd1b55006e29e69c2
Elisa <elisagonzalez@live.nl>
2023-02-11 22:02:20 +0100

    added moment.js timezone config

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,29 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesMoment = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = moment().format(
+losAngelesDateElement.innerHTML = losAngelesMoment.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesMoment.format(
   "hh:mm:ss [<small>]A[</small>]"
 );
 
 let amsterdamElement = document.querySelector("#amsterdam");
 let amsterdamDateElement = amsterdamElement.querySelector(".date");
 let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+let amsterdamMoment = moment().tz("Europe/Amsterdam");
 
-amsterdamDateElement.innerHTML = moment().format("MMMM Do YYYY");
-amsterdamTimeElement.innerHTML = moment().format(
+amsterdamDateElement.innerHTML = amsterdamMoment.format("MMMM Do YYYY");
+amsterdamTimeElement.innerHTML = amsterdamMoment.format(
   "hh:mm:ss [<small>]A[</small>]"
 );
 
 let tokyoElement = document.querySelector("#tokyo");
 let tokyoDateElement = tokyoElement.querySelector(".date");
 let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoMoment = moment().tz("Asia/Tokyo");
 
-tokyoDateElement.innerHTML = moment().format("MMMM Do YYYY");
-tokyoTimeElement.innerHTML = moment().format("hh:mm:ss [<small>]A[</small>]");
+tokyoDateElement.innerHTML = tokyoMoment.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoMoment.format(
+  "hh:mm:ss [<small>]A[</small>]"
+);
